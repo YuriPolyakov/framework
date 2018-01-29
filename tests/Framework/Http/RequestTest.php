39cae14371dade0901c0c5b037867d6c504709d0
Yuriy Polyakov <2104111@dantser.ru>
2018-01-29 12:45:42 +0300

    Add test to class Request

---
+++ tests/Framework/Http/RequestTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Framework\Http;
+
+use Framework\Http\Request;
+use PHPUnit\Framework\TestCase;
+
+class RequestTest extends TestCase
+{
+    public function testEmpty(): void
+    {
+        $request = new Request();
+
+        self::assertEquals([], $request->getQueryParrams());
+        self::assertNull($request->getParsedBody());
+    }
+
+    public function testQueryParams(): void
+    {
+        $request = new Request($data = [
+            'name' => 'John',
+            'age'  => 30
+        ]);
+
+        self::assertEquals($data, $request->getQueryParrams());
+        self::assertNull($request->getParsedBody());
+    }
+
+    public function testParseBody(): void
+    {
+        $request = new Request([], $data = ['title' => 'title']);
+
+        self::assertEquals([], $request->getQueryParrams());
+        self::assertEquals($data, $request->getParsedBody());
+    }
+}
