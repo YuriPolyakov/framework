5b2a343ef82dce8d8a8e67dd2e971a0582976a2b
Yuriy Polyakov <2104111@dantser.ru>
2018-02-01 18:24:36 +0300

    Add tests

---
+++ tests/App/Http/Action/HelloActionTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\App\Http\Action;
+
+use App\Http\Action\HelloAction;
+use PHPUnit\Framework\TestCase;
+use Zend\Diactoros\ServerRequest;
+
+class HelloActionTest extends TestCase
+{
+    public function testGuest()
+    {
+        $action   = new HelloAction();
+        $request  = new ServerRequest();
+        $response = $action($request);
+
+        self::assertEquals(200, $response->getStatusCode());
+        self::assertEquals('Guest', $response->getBody()->getContents());
+    }
+
+    public function testJson()
+    {
+        $action   = new HelloAction();
+        $request  = (new ServerRequest())->withQueryParams(['name' => 'John']);
+        $response = $action($request);
+
+        self::assertEquals('John', $response->getBody()->getContents());
+    }
+}
