1249b01ac7a5cb802c7d94e59fdfcfd831190676
Yuriy Polyakov <2104111@dantser.ru>
2018-02-02 11:55:49 +0300

    Add middleware

--- public/index.php
+++ public/index.php
@@ -7,11 +7,17 @@ use Zend\Diactoros\Response\SapiEmitter;
 use Framework\Http\Router\AuraRouterAdapter;
 use \Framework\Http\Router\Exception\RequestNotMatchedException;
 use Zend\Diactoros\Response\HtmlResponse;
+use Psr\Http\Message\ServerRequestInterface;
 
 chdir(dirname(__DIR__));
 require 'vendor/autoload.php';
 
 // Initialization
+
+$params = [
+    'users' => ['admin' => 'password']
+];
+
 $aura   = new Aura\Router\RouterContainer;
 $routes = $aura->getMap();
 
@@ -21,6 +27,18 @@ $routes->get('about', '/about', function () {
 });
 $routes->get('blog', '/blog', \App\Http\Action\Blog\IndexAction::class);
 $routes->get('blog_show', '/blog/{id}', \App\Http\Action\Blog\ShowAction::class)->tokens(['id' => '\d+']);
+
+$routes->get('cabinet', '/cabinet', function (ServerRequestInterface $request) use ($params) {
+    $profiler = new \App\Http\Middleware\ProfilerMiddleware();
+    $auth     = new \App\Http\Middleware\BasicAuthMiddleware($params['users']);
+    $cabinet  = new \App\Http\Action\CabinetAction();
+
+    return $profiler($request, function (ServerRequestInterface $request) use ($auth, $cabinet) {
+        return $auth($request, function (ServerRequestInterface $request) use ($cabinet) {
+            return $cabinet($request);
+        });
+    });
+});
 
 $router   = new AuraRouterAdapter($aura);
 $resolver = new ActionResolver();
@@ -34,7 +52,7 @@ try {
         $request = $request->withAttribute($attribute, $value);
     }
 
-    $action = $resolver->resolve($result->getHandler());
+    $action   = $resolver->resolve($result->getHandler());
     $response = $action($request);
 } catch (RequestNotMatchedException $e) {
     $response = new JsonResponse(['error' => 'undefined page'], 404);
---
+++ src/App/Http/Action/CabinetAction.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Action;
+
+use App\Http\Middleware\BasicAuthMiddleware;
+use Psr\Http\Message\ServerRequestInterface;
+use Zend\Diactoros\Response\HtmlResponse;
+
+class CabinetAction
+{
+    public function __invoke(ServerRequestInterface $request)
+    {
+        $username = $request->getAttribute(BasicAuthMiddleware::ATTRIBUTE);
+
+        return new HtmlResponse('I am logged is a ' . $username);
+    }
+}
