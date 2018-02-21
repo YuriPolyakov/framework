7fd042762f060b5c12da2989f593c6337f4dda3b
Yuriy Polyakov <2104111@dantser.ru>
2018-02-21 16:48:41 +0300

    Create midleware and pipe. Learn 4/6 full

--- public/index.php
+++ public/index.php
@@ -10,16 +10,11 @@ use Framework\Http\Application;
 use Framework\Http\Middleware\DispatchMiddleware;
 use Framework\Http\Middleware\RouteMiddleware;
 use Framework\Http\Pipeline\MiddlewareResolver;
-use Framework\Http\Pipeline\Pipeline;
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
 use Zend\Diactoros\Response;
-use Zend\Diactoros\Response\JsonResponse;
 use Zend\Diactoros\ServerRequestFactory;
 use Zend\Diactoros\Response\SapiEmitter;
 use Framework\Http\Router\AuraRouterAdapter;
-use \Framework\Http\Router\Exception\RequestNotMatchedException;
-use Zend\Diactoros\Response\HtmlResponse;
+
 
 function dd($value)
 {
@@ -42,25 +37,19 @@ $aura   = new Aura\Router\RouterContainer;
 $routes = $aura->getMap();
 
 $routes->get('home', '/', App\Http\Action\HelloAction::class);
-$routes->get('about', '/about', function () {
-    return new HtmlResponse('about', 200);
-});
 $routes->get('blog', '/blog', \App\Http\Action\Blog\IndexAction::class);
 $routes->get('blog_show', '/blog/{id}', \App\Http\Action\Blog\ShowAction::class)->tokens(['id' => '\d+']);
-$routes->get('cabinet', '/cabinet', [
-    ProfilerMiddleware::class,
-    new BasicAuthMiddleware($params['users']),
-    CabinetAction::class
-]);
+$routes->get('cabinet', '/cabinet',CabinetAction::class);
 
 $router   = new AuraRouterAdapter($aura);
 $resolver = new MiddlewareResolver();
-$app      = new Application($resolver, new NotFoundHadler());
+$app      = new Application($resolver, new NotFoundHadler(), new Response());
 
-$app->pipe(new ErrorHandlerMiddleware($params['debug']));
+//$app->pipe(new ErrorHandlerMiddleware($params['debug']));
 $app->pipe(CredentialsMiddleware::class);
 $app->pipe(ProfilerMiddleware::class);
 $app->pipe(new RouteMiddleware($router));
+$app->pipe('cabinet', new BasicAuthMiddleware($params['users']));
 $app->pipe(new DispatchMiddleware($resolver));
 
 // Running
--- src/Framework/Http/Pipeline/MiddlewareResolver.php
+++ src/Framework/Http/Pipeline/MiddlewareResolver.php
@@ -5,47 +5,39 @@ namespace Framework\Http\Pipeline;
 use Interop\Http\Server\MiddlewareInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Zend\Stratigility\MiddlewarePipe;
 
 class MiddlewareResolver
 {
-    public function resolve($handler): callable
+    public function resolve($handler, ResponseInterface $responsePrototype): callable
     {
         if (\is_array($handler)) {
-            return $this->createPipe($handler);
+            return $this->createPipe($handler, $responsePrototype);
         }
 
         if (\is_string($handler)) {
-            return function (ServerRequestInterface $request, ResponseInterface $response, callable $next) use ($handler
-            ) {
-                $middleware = $this->resolve(new $handler());
-
+            return function (ServerRequestInterface $request, ResponseInterface $response, callable $next) use ($handler) {
+                $middleware = $this->resolve(new $handler(), $response);
                 return $middleware($request, $response, $next);
             };
         }
 
         if ($handler instanceof MiddlewareInterface) {
-            return function (ServerRequestInterface $request, ResponseInterface $response, callable $next) use ($handler
-            ) {
+            return function (ServerRequestInterface $request, ResponseInterface $response, callable $next) use ($handler) {
                 return $handler->process($request, new InteropHandlerWrapper($next));
             };
         }
 
         if (\is_object($handler)) {
             $reflection = new \ReflectionObject($handler);
-
             if ($reflection->hasMethod('__invoke')) {
-                $method     = $reflection->getMethod('__invoke');
+                $method = $reflection->getMethod('__invoke');
                 $parameters = $method->getParameters();
-
                 if (\count($parameters) === 2 && $parameters[1]->isCallable()) {
-                    // TODO обязательный 2 параметр. Не используется
-                    return function (ServerRequestInterface $request, ResponseInterface $response, callable $next) use (
-                        $handler
-                    ) {
+                    return function (ServerRequestInterface $request, ResponseInterface $response, callable $next) use ($handler) {
                         return $handler($request, $next);
                     };
                 }
-
                 return $handler;
             }
         }
@@ -53,13 +45,15 @@ class MiddlewareResolver
         throw new UnknownMiddlewareTypeException($handler);
     }
 
-    public function createPipe(array $handlers): Pipeline
+    public function createPipe(array $handlers, $responsePrototype): MiddlewarePipe
     {
-        $pipeline = new Pipeline();
+        $pipeline = new MiddlewarePipe();
+        $pipeline->setResponsePrototype($responsePrototype);
         foreach ($handlers as $handler) {
-            $pipeline->pipe($this->resolve($handler));
+            $pipeline->pipe($this->resolve($handler, $responsePrototype));
         }
 
         return $pipeline;
     }
 }
+
